db5e9d049b18ff9a55f89c71d1c34cdaa6205ab6
unknown <saehanardelino@gmail.com>
2021-11-09 10:19:19 +0700

    Menambahkan Program dan SS

---
+++ latihan2.py.py
@@ -0,0 +1,9 @@
+bil1 = int(input("MASUKAN BILANGAN KE-1 :"))
+bil2 = int(input("MASUKAN BILANGAN KE-2 :"))
+bil3 = int(input("MASUKAN BILANGAN KE-3 :"))
+terbesar = bil1
+if(bil2>terbesar):
+    terbesar=bil2
+if(bil3>terbesar):
+    terbesar=bil3
+print("BILANGAN TERBESAR DARI KE-3 BILANGAN TERSEBUT ADALAH :", terbesar)
